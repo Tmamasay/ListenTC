dcbb2475723655d9360d6bef35e978f21dfb7293
Tmamasay <1056421380@qq.com>
2020-03-17 22:01:58 +0800

    我的界面及学堂
    Signed-off-by: Tmamasay <1056421380@qq.com>

--- src/pages/mine/eyes/index.vue
+++ src/pages/mine/eyes/index.vue
@@ -1,5 +1,13 @@
 <template>
-  <div class></div>
+  <div>
+    <div class="mineTop">
+      <img src="" alt="">
+    </div>
+    <div class="bookTitle">
+        <p class="leftBook"></p>
+        <p class="leftBookName">温故而知新</p>
+      </div>
+  </div>
 </template>
 
 <script>
@@ -20,4 +28,36 @@ export default {
 </script>
 
 <style scoped>
+.mineTop{
+  width:95%;
+height:70px;
+margin: 16px auto 0 auto;
+background-color: cornflowerblue;
+box-shadow:0px 0px 9px 2px rgba(33,22,19,0.1);
+border-radius:5px;
+}
+.bookTitle{
+  width: 95%;
+  padding-top: 17px;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: row;
+  align-items:center
+  
+}
+  .leftBook{
+     width: 4px;
+     height: 17px;
+     background-color: #F3D24E ;
+   }
+  .leftBookName{
+    /* width:79px;
+    height:38px; */
+    font-size:17px;
+    font-family:Microsoft YaHei;
+    font-weight:bold;
+    color:rgba(48,48,48,1);
+    line-height:30px;
+    margin-left: 9px;
+  }
 </style>
--- src/pages/mine/index.vue
+++ src/pages/mine/index.vue
@@ -11,9 +11,36 @@
       </div>
       </div>
       <div class="powerLine">
-
-
+        <div class="leftBPow">
+          <div class="leftBpImg">
+            <img src="" alt="" srcset="">
+          </div>
+          <p class="powerLineTitle">书房</p>
+        </div>
+        <p class="lineS"></p>
+        <div class="leftBPow">
+           <div class="leftBpImg">
+            <img src="" alt="" srcset="">
+          </div>
+          <p class="powerLineTitle">收藏</p>
+        </div>
       </div>
+       <div class="bookTitle">
+        <p class="leftBook"></p>
+        <p class="leftBookName">温故而知新</p>
+      </div>
+       <div class="bookListCont">
+      <div class="showBookList">
+      <div class="bookLine">
+      </div>
+      <div class="bookLine">
+      </div>
+      <div class="bookLine">
+      </div>
+      </div>
+      <div class="shaowBook"></div>
+      <div class="shaowBookTai"></div>
+    </div>
 
 
     </div>
@@ -51,9 +78,9 @@ export default {
 <style scoped>
 .informationContTop{
 width:100%;
-height:170px;
+height:150px;
 background:rgba(255,217,72,1);
-border-radius:0px 0px 10% 10%;
+border-radius:0px 0px 20% 20%;
 }
 .avterCont{
 
@@ -81,6 +108,7 @@ margin-left: 18px;
   justify-content: flex-start;
   margin-left: 13px;
 
+
 }
 .infoname{
 font-size:19px;
@@ -100,11 +128,103 @@ color:rgba(95,95,95,1);
 .powerLine{
   width:95%;
 height:50px;
-margin: 0 auto;
+margin: 20px auto 0 auto;
 background:rgba(255,255,255,1);
 box-shadow:0px 0px 9px 2px rgba(33,22,19,0.2);
 border-radius:5px;
+display: flex;
+justify-content: space-around;
+align-items:center
 
 }
+.leftBPow{
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+}
+.leftBpImg{
+  width: 17px;
+  height: 19px;
+  background-color: burlywood;
+}
+.leftBpImg img{
+  width: 100%;
+  height: 100%;
+}
+.powerLineTitle{
+ 
+font-size:15px;
+font-family:Microsoft YaHei;
+font-weight:400;
+color:rgba(34,34,34,1);
+margin-left: 7px;
+}
+.lineS{
+  width:1px;
+height:32px;
+border-left: 1px dashed #FFD94B;
+font-size:10px;
+font-family:SimSun;
+font-weight:400;
+/* background-color: #FFD94B; */
+color:rgba(210,210,210,1);
+line-height:42px;
+}
+.bookTitle{
+  width: 95%;
+  padding-top: 17px;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: row;
+  align-items:center
+  
+}
+  .leftBook{
+     width: 4px;
+     height: 17px;
+     background-color: #F3D24E ;
+   }
+  .leftBookName{
+    /* width:79px;
+    height:38px; */
+    font-size:17px;
+    font-family:Microsoft YaHei;
+    font-weight:bold;
+    color:rgba(48,48,48,1);
+    line-height:30px;
+    margin-left: 9px;
+  }
+  .showBookList{
+    width: 95%;
+    margin: 0 auto;
+    display: flex;
+    justify-content: space-around;
 
+  }
+  .bookLine{
+    width: 29%;
+    height: 140px;
+    background:rgba(171,171,184,1);
+    border-radius:5px;
+    margin-top: 24px;
+    margin-bottom: -4px;
+    z-index: 900;
+    
+  }
+  .shaowBook{
+    width: 95%;
+    margin: 0 auto;
+    height:13px;
+    background:rgba(235,235,235,1); 
+    transform: perspective(12px)  rotateX(0.7deg)
+    /* border-left: 20px solid transparent;
+    border-right: 20px solid transparent; */
+  }
+  .shaowBookTai{
+    width: 95%;
+    margin: 0 auto;
+    height:17px;
+    background:rgba(254,254,254,1);
+    box-shadow:0px 0px 6px 0px rgba(33,22,19,0.15);
+  }
 </style>
