f91e3cd3f9ae6847c1c98db3b7aba6169869d32c
Tmamasay <1056421380@qq.com>
2020-03-23 22:00:29 +0800

    书屋、学堂部分收尾完成
    Signed-off-by: Tmamasay <1056421380@qq.com>

--- src/pages/read/catalog/index.vue
+++ src/pages/read/catalog/index.vue
@@ -31,10 +31,45 @@
          <div class="cataImg">
            <img src="../../../../static/images/play/incLine.png" alt="" srcset="">
          </div>
-         <p class="cataContext">播放全部<span>（1043 人已收听）</span></p>
+         <p class="cataContext">播放全部</p>
+         <span>（1043 人已收听）</span>
        </div>
-
+       <div class="cataContList">
+          <div class="cataImg">
+           <img src="../../../../static/images/play/incLine.png" alt="" srcset="">
+         </div>
+         <div class="cataST">
+           <p>1. 汗水</p>
+           <div class="sTBtn">
+             <p class="sTBtnLine">免费试听</p>
+             <div class="sTImg">
+               <img src="" alt="" srcset="">
+             </div>
+             <p class="sTime">2'20''</p>
+           </div>
+         </div>
+       </div>
+        <div class="cataContList">
+          <div class="cataImg">
+           <img src="../../../../static/images/play/incLine.png" alt="" srcset="">
+         </div>
+         <div class="cataST">
+           <p>1. 汗水</p>
+           <div class="sTBtn">
+             <!-- <p class="sTBtnLine">免费试听</p> -->
+             <div class="sTImg">
+               <img src="" alt="" srcset="">
+             </div>
+             <p class="sTime">2'20''</p>
+           </div>
+         </div>
+       </div>
     </div>
+     <div class="footerBtn">
+         <p class="btnLeft">返回</p>
+         <p class="btnRight">立即订阅</p>
+
+       </div>
     
   </div>
 </template>
@@ -53,6 +88,11 @@ export default {
     };
   },
   methods: {
+    handleChange(detail){
+      console.log(detail.mp.detail.key)
+      this.checkItem=detail.mp.detail.key
+      // debugger
+    }
     
   },
   onShow() {
@@ -119,12 +159,17 @@ export default {
   }
   .cataContLine{
     width: 95%;
+    height: 60px;
+    margin: 0 auto;
     display: flex;
+    align-items: center;
+    border-bottom:1px dashed rgba(238,237,237,1);
 
   }
   .cataImg{
     width: 28px;
     height: 28px;
+    margin: auto 9px auto 9px ;
   }
   .cataImg img{
     width: 100%;
@@ -138,14 +183,105 @@ export default {
     font-weight:400;
     color:rgba(34,34,34,1);
   }
-  .cataContext span{
+  .cataContLine span{
     /* width:157px;
     height:21px; */
+    display: inline-block;
     font-size:10px;
     font-family:Microsoft YaHei;
     font-weight:400;
     color:rgba(160,160,160,1);
-    line-height:24px;
+    /* line-height:24px; */
   }
+  .cataContList{
+     width: 95%;
+    height: 82px;
+    margin: 0 auto;
+    display: flex;
+    align-items: center;
+    border-bottom:1px dashed rgba(238,237,237,1)  ;
+  }
+  .cataST{
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+  }
+  .cataST>p{
+    font-size:15px;
+    font-family:Microsoft YaHei;
+    font-weight:400;
+    color:rgba(34,34,34,1);
 
+  }
+  .sTBtn{
+    display: flex;
+    align-items:center;
+    margin-top: 8px;
+  }
+  .sTBtnLine{
+    font-size:10px;
+    font-family:Microsoft YaHei;
+    font-weight:400;
+    color:rgba(67,67,67,1);
+    padding: 5px 8px;
+    background-color: rgba(255,217,72,1);
+    border-radius:19px;
+  
+    margin-right: 10px;
+    /* margin: 13px  auto  auto 10px; */
+  }
+  .sTImg{
+    width: 12px;
+    height: 12px;
+    background-color: cornflowerblue;
+   
+  }
+  .sTImg img{
+    width: 100%;
+    height: 100%;
+  }
+  .sTime{
+    font-size:11px;
+    font-family:Microsoft YaHei;
+    font-weight:400;
+    color:rgba(160,160,160,1);
+     margin-left: 5px;
+  }
+.footerBtn{
+  width:100%;
+height:63px;
+margin: 0 auto;
+background:rgba(255,255,255,1);
+box-shadow:0px 0px 9px 1px rgba(33,22,19,0.2);
+display: flex;
+justify-content:space-around;
+align-items: center;
+position: fixed;
+bottom: 0px;
+}
+.btnLeft{
+width:170px;
+height:45px;
+background:rgba(232,232,232,1);
+border-radius:45px;
+font-size:15px;
+font-family:Microsoft YaHei;
+font-weight:bold;
+color:rgba(34,34,34,1);
+text-align: center;
+line-height:45px ;
+}
+.btnRight{
+  width:170px;
+height:45px;
+background:rgba(255,217,72,1);
+border-radius:45px;
+font-size:15px;
+font-family:Microsoft YaHei;
+font-weight:bold;
+color:rgba(34,34,34,1);
+text-align: center;
+line-height:45px ;
+
+}
 </style>
